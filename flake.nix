0c6813043203cd58818c277afade1bebe47f2eba
Greg Pfeil <greg@technomadic.org>
2026-03-05 07:43:50 -0700

    Pathway branch was merged

--- flake.nix
+++ flake.nix
@@ -42,8 +42,7 @@
 
     pathway = {
       inputs.flaky.follows = "flaky";
-      ## NB: The revision here needs to match the one in ./cabal.project
-      url = "github:sellout/pathway/system-directories";
+      url = "github:sellout/pathway";
     };
   };
 }
